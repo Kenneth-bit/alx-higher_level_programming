817554ac88d06dde6ac1faf98e6f0836f910d015
Kenneth-bit <mpumelelomofokengkenneth@gmail.com>
2021-09-09 09:43:26 +0000

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(26):
-    if alphabet != 4 and alphabet != 16:
-       print("{:s}".format(chr(ord('a') + alphabet))) 
+for alpha in range(26):
+    if alpha != 4 and alphabet != 16:
+       print("{:s}".format(chr(ord('a') + alpha))) 
