f7ab5b474fbf8de5008ab09f42701aea50f6ba4c
Kenneth-bit <mpumelelomofokengkenneth@gmail.com>
2021-09-09 12:46:28 +0000

    Upload __pycache__ and add_0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition Function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        a + b
+    """
+
+    return(a + b)
