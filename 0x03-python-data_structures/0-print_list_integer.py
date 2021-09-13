0a442760853bec24ac998bbf8e50560c62edf8f6
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-09-13 07:26:49 -0700

    Create 0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+	""" This functions prints all\
+		integers in a list
+
+	    Return : Integers in the list, on per line.
+	"""
+	for num in my_list:
+	    print(str.format(str(num)))
