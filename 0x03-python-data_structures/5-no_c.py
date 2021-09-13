76ee7147a044636b8d82a08ea3f7f53470cae133
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-09-13 13:08:27 -0700

    Crete 5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+	"""Replace c and C in a string
+	"""
+	word = " "
+	for char in my_string:
+	    if char != 'c' and char != 'C':
+	        word = word + char
+	return word
