7af36f1824d9fa09037694cbeac00afc1c6d1678
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-09-25 13:30:15 -0700

    Create 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for alpha in range(26):
-    if alpha != 4 and alpha != 16:
-       print("{:s}".format(chr(ord('a') + alpha)), end="") 
+for alpha in range(97, 123):
+	print(chr(alpha), end='')
