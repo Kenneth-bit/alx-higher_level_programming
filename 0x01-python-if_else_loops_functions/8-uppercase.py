3c76e1cf5e859209d7936c6d690ffdcb179545f0
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-03 12:22:49 -0700

    Create 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            print("{}".format(chr(ord(char) - 32)), end='')
+        if ord(char) >= 97 and ord(char) < 123:
+           cap_str = chr(ord(char) - 32)
         else:
-            print("{}".format(char), end='')
-    print('')
+           cap_str = char
+        print("{}".format(cap_str), end='')
+    print()
