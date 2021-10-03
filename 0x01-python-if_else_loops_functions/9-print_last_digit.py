ffe63c354d4679d789e8934a2e4895ab04dca777
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-03 12:55:15 -0700

    Create 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	num = str(number)
-	print(num[-1])
-
-print_last_digit(101)
+	if number < 0:
+	    num = number * -1
+	    last_digit = num % 10
+	else:
+	    num = number
+	    last_digit = num % 10
+	print("{}".format(last_digit), end='')
+	return last_digit
