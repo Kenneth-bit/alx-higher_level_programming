74e1ed3c72b97e8dc68159671d62f925868abf7f
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-10-03 13:02:09 -0700

    Fix pep8 indentation error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	if number < 0:
-	    num = number * -1
-	    last_digit = num % 10
-	else:
-	    num = number
-	    last_digit = num % 10
-	print("{}".format(last_digit), end='')
-	return last_digit
+    if number < 0:
+        num = number * -1
+        last_digit = num % 10
+    else:
+        num = number
+        last_digit = num % 10
+    print("{}".format(last_digit), end='')
+    return last_digit
