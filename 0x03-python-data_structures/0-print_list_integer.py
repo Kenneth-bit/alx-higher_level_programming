93cc3f1db745a40d5846e00f517ed0efb4a7fed4
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-04 05:32:42 -0800

    PEP 8 pycodestyle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	""" This functions prints all\
-		integers in a list
-
-	    Return : Integers in the list, one per line.
-	"""
-	for num in my_list:
-	    print('{:d}'.format(num))
+    """ This functions prints all integers in a list
+        Return : Integers in the list, one per line.
+    """
+    for num in my_list:
+        print('{:d}'.format(num))
