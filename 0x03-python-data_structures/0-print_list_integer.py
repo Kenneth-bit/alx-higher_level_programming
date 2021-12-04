026b77095d74fd7765b7150a463ca06e527a1725
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-04 03:22:20 -0800

    Remove str()

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,7 @@ def print_list_integer(my_list=[]):
 	""" This functions prints all\
 		integers in a list
 
-	    Return : Integers in the list, on per line.
+	    Return : Integers in the list, one per line.
 	"""
 	for num in my_list:
-	    print(str.format(str(num)))
+	    print('{}'.format(num))
