b0928033387733bdf0d842d1bee17215ec365a38
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-05 09:58:00 -0800

    Reformat (PEP 8)

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	"""Replace c and C in a string
-	"""
-	word = ""
-	for char in my_string:
-	    if char != 'c' and char != 'C':
-	        word = word + char
-	return word
+    """Replace c and C in a string"""
+    word = ""
+    for char in my_string:
+        if char != "c" and char != "C":
+            word = word + char
+    return word
