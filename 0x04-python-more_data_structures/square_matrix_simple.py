1e634eb7cbded7d901c95f06873011729c3843a8
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-10 06:27:45 -0800

    Remove __pycache__

--- 0x04-python-more_data_structures/square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    resulting_matrix = []
-    for value in matrix:
-        new_matrix = []
-        for number in value:
-            new_matrix.append(number**2)
-        resulting_matrix.append(new_matrix)
-    return resulting_matrix
