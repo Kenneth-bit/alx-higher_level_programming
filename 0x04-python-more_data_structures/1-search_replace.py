f6a56493cf2fb3ea5fe8469fe4bca575ce8da871
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-10 06:51:38 -0800

    Update for loop

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for number in my_list:
+    for number in range(1, len(my_list) + 1):
         if search in my_list:
             new_list.insert(search, replace)
         else:
