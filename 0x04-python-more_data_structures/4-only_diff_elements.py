acd1d1c02067074013588d62264952c9afc4cc65
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 02:58:06 -0800

    Rename variables

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-    return a ^ b
+    list_1 = set(set_1)
+    list_2 = set(set_2)
+    return list_1 ^ list_2
