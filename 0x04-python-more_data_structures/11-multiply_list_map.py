14c15bfdcc18d431d9104b124cac2440e9dbe156
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 08:20:55 -0800

    Remove extra lines

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], factor=None):
-    return list(map(lambda number: number * factor, my_list))
+def multiply_list_map(my_list=[], factor=None): return list(map(lambda number: number * factor, my_list))
