ade926a3151e64dab141b6ccb26345d34f0dc148
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 03:48:29 -0800

    Reformat

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    m = sorted(a_dictionary.values())
-    value_max = m[-1]
-    for i in a_dictionary:
-        if value_max == a_dictionary[i]:
-            return i
+    highscore = max(a_dictionary)
+    return highscore 
