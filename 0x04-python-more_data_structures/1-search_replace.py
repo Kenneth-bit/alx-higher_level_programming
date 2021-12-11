ac5fe02c64b09b903ceb91cf499d8da3dbb9f0b3
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 02:22:19 -0800

    Add function documentation

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+	''' search_replace --  replaces all occurrences of an element by another in a new list.
+	    new_list -- modified list.
+	    my_list -- input list.
+	    search -- element to be replaced.
+	    replace -- element to be inserted.
+        '''
     new_list = my_list.copy()
     for number in range(len(my_list)):
         if new_list[number] == search:
             new_list[number] = replace
     return new_list
-
-
-
-#def search_replace(my_list, search, replace):
-#    def s_r_elm(elm):
-#        return (elm if elm != search else replace)
-#    return list(map(s_r_elm, my_list))
