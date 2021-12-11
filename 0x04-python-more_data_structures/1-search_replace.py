14537d677de64a666c10e551b42566e760847455
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 01:38:53 -0800

    Fix for loop logic

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for number in range(1, len(my_list) + 1):
-        if number == search:
-            new_list.insert(search, replace)
-        else:
-            new_list.append(number)
+    new_list = my_list.copy()
+    for number in range(len(my_list)):
+        if new_list[number] == search:
+            new_list[number] = replace
     return new_list
+
+
+
+#def search_replace(my_list, search, replace):
+#    def s_r_elm(elm):
+#        return (elm if elm != search else replace)
+#    return list(map(s_r_elm, my_list))
