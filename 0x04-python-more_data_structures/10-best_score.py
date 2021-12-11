d8a5ad441c1987c0b6a12898675069686aea3f30
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-11 04:01:31 -0800

    Fix return statement

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    highscore = max(a_dictionary)
+    highscore = max(a_dictionary, key=a_dictionary.get)
     return highscore 
