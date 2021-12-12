f078cdd40ca9163c3e0a5e18beadd2115f66854a
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-12 04:38:44 -0800

    Reformat with black

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    w_average = 0
-    total_score = 0
-    total_weight = 0
-    for tuple in range(len(my_list)):
-        values = my_list[tuple]
-        score, weight = values
-        total_weight += weight
-        product = weight * score
-        total_score += product
-    w_average = total_score / total_weight
-    return w_average
+    if my_list == []:
+        return 0
+    else:
+        total_score = 0
+        total_weight = 0
+        for tuple in range(len(my_list)):
+            values = my_list[tuple]
+            score, weight = values
+            total_weight += weight
+            product = weight * score
+            total_score += product
+        w_average = total_score / total_weight
+        return w_average
+
+
 weight_average([(1, 2), (2, 1), (3, 10), (4, 2)])
