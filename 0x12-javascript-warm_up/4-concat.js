ec12d893cdbd3075f25adba20233e8fa0bb6f122
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-14 05:56:17 -0800

    console.log undefined

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 const argv = require('process').argv;
-console.log(argv[2], 'is', argv[3])
+if (argv.length === 2){
+  console.log('undefined is undefined');
+} else if (argv.length === 3){
+  console.log(argv[2], 'is undefined');
+} else {
+  console.log(argv[2], 'is', argv[3]);
+}
