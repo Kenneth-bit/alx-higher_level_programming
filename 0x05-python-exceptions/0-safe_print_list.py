4ac98e90f4ea6d3433cc838118ec171903b9aaad
Kenneth Mpumelelo Mfokeng <mpumelelomofokengkenneth@gmail.com>
2021-12-24 03:05:51 -0800

    Use .format()

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         num_elem = 0
         for num in range(x):
-            print(my_list[num], end="")
+            print("{}".format(my_list[num]), end="")
             num_elem += 1
         print("")
     except IndexError:
